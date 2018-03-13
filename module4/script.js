fb9e2e21d45218a58c613082417fd078b5f4a8cb
zugelek <36538838+zugelek@users.noreply.github.com>
2018-03-13 23:30:22 +0300

    Update script.js

--- module4/script.js
+++ module4/script.js
@@ -31,7 +31,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+(function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
@@ -57,10 +57,10 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter === 'j') {
-    window.byeSpeaker.speak(names[i]);
+    byeSpeaker.speak(names[i]);
   } else {
-    window.helloSpeaker.speak(names[i]);
+    helloSpeaker.speak(names[i]);
   }
 }
 
-})(window);
+})();
